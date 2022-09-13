1891456adb8ab877a377e67eb6f4ee944ed5ec6d
Ivan Mayo <56654050+ivamayo@users.noreply.github.com>
2022-09-13 15:37:03 -0500

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import tipodatos.TipoDato;
+
+public class Main {
+    public static void main(String[] args){
+
+        int number1= 1;
+
+        TipoDato.DatoInt(number1);
+
+        long long1 = 99999;
+
+        TipoDato.DatoLong(long1);
+
+        double decimal = 9.99d;
+
+        TipoDato.DatoDouble(decimal);
+
+        boolean codigo = true;
+
+        TipoDato.DatoBoolean(codigo);
+
+        String nombre = "IVAN";
+
+        TipoDato.DatoString(nombre);
+
+    }
+}
---
+++ src/tipodatos/TipoDato.java
@@ -0,0 +1,31 @@
+package tipodatos;
+
+public class TipoDato {
+
+    //int
+    public static void DatoInt(int valor)
+    {
+        System.out.println("El valor numero es " + valor);
+    }
+
+    public static void DatoString( String valor)
+    {
+        System.out.println("El valor string es " + valor);
+    }
+
+    public static void DatoLong(long valor)
+    {
+        System.out.println("El valor long es "+ valor);
+    }
+
+    public static void DatoDouble(double valor)
+    {
+        System.out.println("El valor double es "+ valor);
+    }
+
+    public static void DatoBoolean(boolean valor){
+        System.out.println("El valor boolean es " + valor);
+    }
+
+
+}
